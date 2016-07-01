c84cfaae9b17698246e72a739fa6661385d98d21
Otis Wright <otis@beardedweb.com>
2016-07-01 14:53:04 +1200

    Update vendor_styles.js

--- gulp/tasks/vendor_styles.js
+++ gulp/tasks/vendor_styles.js
@@ -3,12 +3,14 @@ var config = require('../config');
 var gulp  = require('gulp'),
     plumber = require('gulp-plumber'),
     concat = require('gulp-concat'),
-    minify = require('gulp-cssnano');
+    minify = require('gulp-cssnano'),
+    autoprefixer = require('gulp-autoprefixer'),
 
 module.exports = function () {
     return gulp.src(config.files.vend_stylesheets)
         .pipe(plumber())
         .pipe(concat(config.vend_main_css))
+        .pipe(autoprefixer({browsers: config.autoprefix}))
         .pipe(minify({safe: true, discardDuplicates: false, verbose: true}))
         .pipe(gulp.dest(config.dist_stylesheets))
 };
